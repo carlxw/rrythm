cc6a85cefeb12fdee1d4fc5f4e77bfe43f7c275d
Carl W <carlwang019@gmail.com>
2022-02-03 14:32:12 -0500

    Disconnect update

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,6 +1,7 @@
 module.exports = disonnect = (message, client, connection) => {
     // Falsy: There is no connection
     if (!connection) {
+        message.channel.send("❌ **I am not in a voice channel**");
         return;
     } else { // Truthy: There is a connection
         connection.destroy();
