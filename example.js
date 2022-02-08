016afc3f2d382a616bc22b176a69f6e8406bd3d2
Carl W <carlwang019@gmail.com>
2022-02-07 21:29:46 -0500

    Update example.js

--- example.js
+++ example.js
@@ -5,38 +5,45 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.DIRECT_MESSAGES],
     partials: ['CHANNEL', 'MESSAGE']
 })
-const token = 'OTM4MDgxNTExOTcyOTI5NTc3.YflGfQ.w5362OR8hNlgWDrbPLRXxPsDtZ8'
+const token = "OTM4MDgxNTExOTcyOTI5NTc3.YflGfQ.w5362OR8hNlgWDrbPLRXxPsDtZ8";
 
 client.on('messageCreate', async message => {
-
+    
     if (message.content.startsWith('!play')) {
         
-        if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
+        // if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
         
         const connection = joinVoiceChannel({
-            channelId: message.member.voice.channel.id,
+            channelId: "938100003677810729",
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator
         })
 
-        let args = message.content.split('play ')[1].split(' ')[0]
-
-        let stream = await play.stream(args)
         
-        /*
-        OR if you want to get info about youtube link and then stream it
+        let args = message.content.split('play')[1]
 
-        let yt_info = await play.video_info(args)
-        console.log(yt_info.video_details.title) 
-        let stream = await play.stream_from_info(yt_info)
-        */
+        let yt_info = await play.search(args, {
+            limit: 1
+        })
 
+        console.log(yt_info[0].thumbnails);
+        console.log()
+        console.log(yt_info);
+        console.log()
+        console.log(yt_info[0]);
+        
+        let stream = await play.stream(yt_info[0].url)
+        
         let resource = createAudioResource(stream.stream, {
             inputType: stream.type
         })
 
-        let player = createAudioPlayer();
-        
+        let player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Play
+            }
+        })
+
         player.play(resource)
 
         connection.subscribe(player)
