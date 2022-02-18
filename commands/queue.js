83421286372433dbdf6b890158e04057d0490294
Carl W <carlwang019@gmail.com>
2022-02-17 19:33:53 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,13 +1,12 @@
 const Discord = require("../modules/Discord.js");
 
-module.exports = (musicPlayer, message) => {
+module.exports = (message) => {
     const { connection } = require("../index.js");
     musicPlayer = connection.getMusicPlayer();
 
     // User must be in a voice channel to use commands
     if (!message.member.voice.channel) message.channel.send("❌ **You have to be in a voice channel to use this command.**");
-    
-    if (!message.member.voice.channel || message.member.voice.channel.name === musicPlayer.getSetVChannel()) {
+    else {
         const discord = new Discord();
         const embed = discord.embedQueue(musicPlayer, message);
         message.channel.send({embeds: [embed]});
