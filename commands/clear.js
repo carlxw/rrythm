f24f6a477d86d6b780425bf2dd00c01fd88a28c6
carlxw <87335263+carlxw@users.noreply.github.com>
2022-02-20 00:16:43 -0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,11 @@ module.exports = (message) => {
     const { connection } = require("../index.js");
     const musicPlayer = connection.getMusicPlayer();
 
-    musicPlayer.getQueue().clear();
-
-    message.channel.send("💥 ***Cleared...*** ⏹");
+    // User must be in a voice channel to use commands
+    if (!message.member.voice.channel) message.channel.send("❌ **You have to be in a voice channel to use this command.**");
+    else if (message.member.voice.channel.name === musicPlayer.getSetVChannel()) {
+        musicPlayer.getQueue().clear();
+        message.channel.send("💥 ***Cleared...*** ⏹");
+    }
+    
 }
