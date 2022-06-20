cea2f5d16c2cecf5613f487bc3ca37045be6b1bd
Carl W <carlwang019@gmail.com>
2022-06-19 20:03:11 -0400

    Bug fix

--- commands/queue.js
+++ commands/queue.js
@@ -1,8 +1,12 @@
 module.exports = (message, musicPlayer, discord) => {
-    if (musicPlayer.queue.length() < 20) message.channel.send({embeds: [discord.embedQueue(message)]});
+    // One page is needed
+    if (musicPlayer.queue.length() < 20) {
+        message.channel.send({embeds: [discord.embedQueue(message)]});
+        return;
+    }
     
+    // Multiple pages are needed
     const loop = Math.ceil(musicPlayer.queue.length() / 20);
-
     for (let i = 0; i < loop; i++) {
         message.channel.send({embeds: [discord.embedQueue(message, i+1)]});
     }
