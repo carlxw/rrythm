cd864d50e795c65f0234b77f5b75eab935805aca
carlxw <87335263+carlxw@users.noreply.github.com>
2022-12-26 16:43:56 -0500

    Create Launch.py

---
+++ Launch.py
@@ -0,0 +1,24 @@
+import os, urllib.request, datetime, time
+
+command = "node ."
+
+# Returns true if connected to the internet
+def connected():
+	try:
+		urllib.request.urlopen("http://www.google.ca")
+		return True
+	except:
+		return False
+		
+while (True):
+	os.system(command)
+	
+	now = datetime.datetime.now()
+	# Host is disconnected from the internet, retry in 10 mins
+	if (not connected()):
+		print("Disconnect occured at " + str(now))
+		time.sleep(600)
+		
+	# Error has occured, restart immediately
+	else:
+		print("An error occured at " + str(now))
